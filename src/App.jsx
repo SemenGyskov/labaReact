bedb2a4754be640e03dd465986dfb6ce92ec7f09
SemenGyskov <114874984+SemenGyskov@users.noreply.github.com>
2023-01-20 12:44:22 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import './App.css'; //подключение css файла
+import { useState } from 'react';
+
+function App() {
+  const initTasks=[
+    'сделать домашнее задание','нарисовать рисунок','помыть кота'
+  ] //переменная с массивом
+  const [edit,setEdit]=useState(null)
+  const[tasks,setTasks]=useState(initTasks)
+
+  const result = tasks.map((task,index) =>{
+    return <li key={index} onClick={()=> startEdit (index)} className=""><input type='checkbox' className='check' />{task} <button className='btn' onClick={()=> remItem(index)}>Удалить</button></li>
+  })
+  function remItem(index){  //Функция для удаления элемента
+    setTasks([...tasks.slice(0, index), ...tasks.slice(index +1)])
+  }
+  function startEdit(index){
+    setEdit(index)
+  }
+  function editItem(event){
+    setTasks([...tasks.slice(0, edit), event.target.value, ...tasks.slice(edit +1)])
+  }
+  function createItem(){ //Функция для создания элемента
+    if (!edit){
+      const res = [...tasks, '']
+      setTasks(res)
+      setEdit(res.length -1)
+    }
+  }
+  function stopEdit(){
+    setEdit(null)
+  }
+  return (
+    <div className="App">
+      <h1>Todos</h1>
+      <input value={edit ? tasks[edit] : ''} onChange={editItem} onFocus={createItem} onBlur={stopEdit} placeholder="Add Todo..." />{/* Инпут, для добавления и изменения элементов */}
+     {result} {/* вывод результата */}
+     
+     
+
+
+    </div>
+  );
+}
+
+export default App;
