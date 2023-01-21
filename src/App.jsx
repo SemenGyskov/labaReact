7460020adba749586557ae3d5a5ccea8862f4741
SemenGyskov <114874984+SemenGyskov@users.noreply.github.com>
2023-01-21 16:38:02 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'; //подключение css файла
-import  Todo  from "./components/Todo"
+import  Todo  from "./components/Todo" //подключение компонента
 function App() {
   return(
     <div>
